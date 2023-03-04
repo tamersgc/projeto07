bab699281b061e814919fbb487b44ed1ec48ffe1
tamersgc <tamersgc@gmail.com>
2023-03-04 18:52:10 -0300

    uploading projetc

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// setInterval(()=>console.log('opaaaa'), 1000)
+// setTimeout(()=>console.log('opaaaa'), 1000)
+
+import { Router } from "./router.js"
+
+
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/about", "/pages/about.html")
+router.add("/contact", "/pages/contact.html")
+router.add(404, "/pages/404.html")
+
+// const routes = {
+//     "/": "/pages/home.html",
+//     "/about": "/pages/about.html",
+//     "/contact": "/pages/contact.html",
+//     404: "/pages/404.html"
+// }
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
